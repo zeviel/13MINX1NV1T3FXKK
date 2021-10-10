c5e6126c15f8b3a6cd47e3182396ff24ed8e835f
LilZevi <77536370+LilZevi@users.noreply.github.com>
2021-10-10 16:32:38 +0600

    Create AminoInviteFxck.py

---
+++ AminoInviteFxck.py
@@ -0,0 +1,20 @@
+import pyfiglet
+from configs import menu_configs, invite_functions
+from tabulate import tabulate
+from colored import fore, back, style, attr
+attr(0)
+print(fore.LIGHT_CORAL + style.BOLD)
+print("""Script by Lil Zevi
+Github : https://github.com/LilZevi""")
+print(pyfiglet.figlet_format("aminoinvitefxck", font="rectangles"))
+print(tabulate(menu_configs.main_menu, tablefmt="psql"))
+select = input("Select >> ")
+
+if select == "1":
+	invite_functions.invite_online_users()
+
+elif select == "2":
+	invite_functions.invite_recent_users()
+
+elif select == "3":
+	invite_functions.invite_user_followers()
