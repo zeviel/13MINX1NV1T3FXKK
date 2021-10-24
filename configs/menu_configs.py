53a1f7bc123cf302e13885e9996a39c7bc55968f
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2021-10-24 09:31:53 +0600

    Update menu_configs.py

--- configs/menu_configs.py
+++ configs/menu_configs.py
@@ -1,6 +1,6 @@
 
 main_menu = [
-    ["Telegram >> ", "https://t.me/aminobotslilzevi"],
+    ["Telegram >> ", "https://t.me/aminobotsdelav"],
     ["1", "Invite Online Users"],
     ["2", "Invite Recent Users"],
     ["3", "Invite User Followers"]
