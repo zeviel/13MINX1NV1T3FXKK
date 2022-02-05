8cf840cb8fc1115d66b616939521919c047035d9
deluvsushi <77536370+deluvsushi@users.noreply.github.com>
2022-02-05 16:06:47 +0600

    Update invite_functions.py

--- configs/invite_functions.py
+++ configs/invite_functions.py
@@ -1,52 +1,39 @@
-import samino
-import concurrent.futures
-client = samino.Client(None)
+import amino
+from concurrent.futures import ThreadPoolExecutor
 
+client = amino.Client()
 
 def auth():
     while True:
         try:
-            email = input("Email >> ")
-            password = input("Password >> ")
+            email = input("-- Email::: ")
+            password = input("-- Password::: ")
             client.login(email=email, password=password)
             return False
         except Exception as e:
             print(e)
-        except BaseException:
-            return
-
+            
 
 def communities():
     try:
-        clients = client.get_my_communitys(size=100)
+        clients = client.sub_clients(size=100)
         for x, name in enumerate(clients.name, 1):
             print(f"{x}.{name}")
         while True:
-            com_Id = clients.comId[int(input("Select the community >> ")) - 1]
-            return com_Id
-    except ValueError:
-        print("ValueError")
-        communities()
-    except Exception as e:
-        print(e)
+            com_id = clients.comId[int(input("Select the community >> ")) - 1]
+            return com_id
     except BaseException:
-        return
+        communities()
 
 
-def chats(local: str):
+def chats(sub_client: amino.SubClient):
     try:
-        chats = local.get_chat_threads(size=100)
+        chats = sub_client.get_chat_threads(size=100)
         for z, title in enumerate(chats.title, 1):
             print(f"{z}.{title}")
         while True:
-            chat_Id = chats.chatId[int(input("Select The Chat >> ")) - 1]
-            return chat_Id
-    except ValueError:
-        print("ValueError")
-        return
-    except Exception as e:
-        print(e)
-        return
+            chat_id = chats.chatId[int(input("Select The Chat >> ")) - 1]
+            return chat_id
     except BaseException:
         return
 
@@ -55,21 +42,21 @@ def chats(local: str):
 
 def invite_online_users():
     auth()
-    local = samino.Local(communities())
-    chat_Id = chats(local)
+    sub_client = amino.SubClient(comId=communities(), profile=client.profile)
+    chat_id = chats(sub_client=sub_client)
     while True:
-        with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
+        with ThreadPoolExecutor(max_workers=100) as executor:
             for i in range(0, 2000, 250):
                 try:
-                	online_users = local.get_online_users(start=i, size=100)
-                	for nickname, user_Id in zip(
+                	online_users = sub_client.get_online_users(start=i, size=100)
+                	for nickname, user_id in zip(
                         online_users.nickname, online_users.userId):
-                        	print(f"{nickname} Invited to chat")
-                        	_ = [
+                        	print(f"-- Invited::: {nickname} to chat!")
+                        	[
                         	executor.submit(
-                        	local.invite_to_chat,
-                        	user_Id,
-                        	chat_Id)]
+                        	sub_client.invite_to_chat,
+                        	user_id,
+                        	chat_id)]
                 except Exception as e:
                       print(e)
 
@@ -78,20 +65,20 @@ def invite_online_users():
 
 def invite_recent_users():
     auth()
-    local = samino.Local(communities())
-    chat_Id = chats(local)
-    with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
-        for i in range(0, 2000, 250):
+    sub_client = amino.SubClient(comId=communities(), profile=client.profile)
+    chat_id = chats(sub_client=sub_client)
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for i in range(0, 2000, 15000):
             try:
-            	recent_users = local.get_all_users(type="recent", start=i, size=100)
-            	for nickname, user_Id in zip(
+            	recent_users = sub_client.get_all_users(type="recent", start=i, size=100)
+            	for nickname, user_id in zip(
                     recent_users.nickname, recent_users.userId):
-                    	print(f"{nickname} Invited to chat")
-                    	_ = [
+                    	print(f"-- Invited::: {nickname} to chat!")
+                    	[
                     	executor.submit(
-                    	local.invite_to_chat,
-                        user_Id,
-                        chat_Id)]
+                    	sub_client.invite_to_chat,
+                        user_id,
+                        chat_id)]
             except Exception as e:
                   print(e)
 
@@ -100,20 +87,20 @@ def invite_recent_users():
 
 def invite_user_followers():
     auth()
-    local = samino.Local(communities())
-    chat_Id = chats(local)
-    user_info = client.get_from_link(input("User Link >> "))
-    with concurrent.futures.ThreadPoolExecutor(max_workers=200) as executor:
-        for i in range(0, 2000, 250):
+    sub_client = amino.SubClient(comId=communities(), profile=client.profile)
+    chat_id = chats(sub_client=sub_client)
+    user_info = client.get_from_code(input("-- User link::: "))
+    with ThreadPoolExecutor(max_workers=100) as executor:
+        for i in range(0, 2000, 15000):
             try:
-            	user_followers = local.get_member_followers(userId=user_info.objectId, start=i, size=100)
-            	for nickname, user_Id in zip(
+            	user_followers = sub_client.get_member_followers(userId=user_info.objectId, start=i, size=100)
+            	for nickname, user_id in zip(
                     user_followers.nickname, user_followers.userId):
-                    	print(f"{nickname} Invited to chat")
-                    	_ = [
+                    	print(f"-- Invited::: {nickname} to chat!")
+                    	[
                     	executor.submit(
-                    	local.invite_to_thread,
-                        user_Id,
-                        chat_Id)]
+                    	sub_client.invite_to_thread,
+                        user_id,
+                        chat_id)]
             except Exception as e:
                     print(e)
