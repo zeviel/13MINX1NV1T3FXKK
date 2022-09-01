05f8fd3388b7060d55970914c568a8f914085312
zeviel <77536370+zeviel@users.noreply.github.com>
2022-09-01 19:14:02 +0500

    Update and rename configs/menu_configs.py to utils/configs.py

--- configs/menu_configs.py
+++
@@ -1,7 +0,0 @@
-
-main_menu = [
-    ["Telegram >> ", "https://t.me/aminobotsdelav"],
-    ["1", "Invite Online Users"],
-    ["2", "Invite Recent Users"],
-    ["3", "Invite User Followers"]
-]
---
+++ utils/configs.py
@@ -0,0 +1,17 @@
+MAIN_MENU = [
+    [1, "invite online users"],
+    [2, "invite recent users"],
+    [3, "invite user followers"]
+]
+
+LOGO = """ 
+\033[38;5;73m                                                                                                                                                                                                                                             
+ #  ### # # ### ### # #  #  ### # #  #  ### ### ### # # # # 
+##    # ###  #  # # # # ##  # # # # ##   #    # #   # # # # 
+ #   ## ###  #  # #  #   #  # # # #  #   #   ## ##   #  ##  
+ #    # # #  #  # # # #  #  # # # #  #   #    # #   # # # # 
+### ### # # ### # # # # ### # #  #  ###  #  ### #   # # # # 
+[Author]::: zeviel
+[GitHub]::: https://github.com/zeviel 
+"""
+
